cd8772593d126e2860b16cca7899977cf8d5e264
jhaskett004 <jhaskett@ualr.edu>
2020-10-09 22:59:04 -0500

    Final

--- app/src/main/java/com/ualr/resources/MainActivity.java
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -2,22 +2,37 @@ package com.ualr.resources;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import android.content.res.Resources;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
-// TODO 3: Define the alternative resources needed to have different content and look n feel depending on the device language.
-// TODO 4: Get the description string value from resources
-// TODO 5: Initialize the text property of the TextView element with the "country_description_text" id by using the value retrieved in the previous TODO point
+public class MainActivity extends AppCompatActivity{
 
-public class MainActivity extends AppCompatActivity {
+    private TextView country_description_text;
+    private Button lesson_learnt_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        country_description_text = findViewById(R.id.country_description_text);
+        lesson_learnt_button = findViewById(R.id.lesson_learnt_button);
+
+        Resources res = getResources();
+        String description = res.getString(R.string.country_description_text);
+        country_description_text.setText(description);
     }
 
+    public void onLessonLearntClicked(View view) {
+        Resources res = getResources();
+        lesson_learnt_button.setBackgroundColor(res.getColor(R.color.colorOnClick));
+    }
 }
+
+
